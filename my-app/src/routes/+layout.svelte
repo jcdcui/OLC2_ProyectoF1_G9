9ffdbe2b4a9b341a1b387f07ae1daab69978c9de
Edmy20 <edmyingenieria@gmail.com>
2024-12-08 18:16:47 -0600

    centrado de texto

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -2,7 +2,9 @@
   import "../app.css";
 </script>
 
-<div class= "w-screen h-screen">
-  <h1 class="bg-blue-100 text-blue-800 text-50px font-semibold me-2 px-10 py-10 rounded dark:bg-blue-200 dark:text-blue-800 ms-2">FortranPEG</h1>
-<slot />
+<div class="w-screen h-screen flex flex-col justify-center items-center">
+  <h1 class="bg-blue-100 text-blue-800 text-6xl font-semibold px-10 py-10 rounded dark:bg-blue-200 dark:text-blue-800">
+    FortranPEG
+  </h1>
+  <slot />
 </div>
