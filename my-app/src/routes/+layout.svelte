6c09e17d14fbab6a915c612c7a9d12cea8a447e6
Edmy20 <edmyingenieria@gmail.com>
2024-12-08 18:07:42 -0600

    font titulo

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <div class= "w-screen h-screen">
-  <h1>FortranPEG</h1>
+  <h1 class="bg-blue-100 text-blue-800 text-50px font-semibold me-2 px-10 py-10 rounded dark:bg-blue-200 dark:text-blue-800 ms-2">FortranPEG</h1>
 <slot />
 </div>
