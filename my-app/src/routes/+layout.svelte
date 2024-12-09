360f9d98db99813ffd084c3e54d90f74c3063c61
Edmy20 <edmyingenieria@gmail.com>
2024-12-08 18:19:33 -0600

    arreglo de consolas

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -2,9 +2,10 @@
   import "../app.css";
 </script>
 
-<div class="w-screen h-screen flex flex-col justify-center items-center">
-  <h1 class="bg-blue-100 text-blue-800 text-6xl font-semibold px-10 py-10 rounded dark:bg-blue-200 dark:text-blue-800">
+<div class="w-screen h-screen">
+  <h1 class="bg-blue-100 text-blue-800 text-6xl font-semibold px-10 py-10 rounded dark:bg-blue-200 dark:text-blue-800 mx-auto">
     FortranPEG
   </h1>
   <slot />
 </div>
+
